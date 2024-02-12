49a64851df2fffc773394079004f7d14a8f88568
Thomas Martinez <thomas.martinez@igt.com>
2024-02-12 18:26:45 -0500

    created mongodb, installed mongoose, connected app to db

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:bHq9cTkbUiy5zK8P@bookstore-mern.ixru4oc.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get('/', (req, res) => {
   return res.status(234).send('Welcome to Book Store');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
