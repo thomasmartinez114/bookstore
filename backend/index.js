0e58cdb0bf6b0cb9c17c8fbda45c471b6e3832bc
Thomas Martinez <thomas.martinez@igt.com>
2024-02-12 18:13:25 -0500

    created first http route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Welcome to Book Store');
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
