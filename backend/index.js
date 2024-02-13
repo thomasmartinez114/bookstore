e85ef04269a74e6119a4f40a4a8dd346b1d73049
Thomas Martinez <thomas.martinez@igt.com>
2024-02-12 22:39:29 -0500

    update book by id

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.get('/books', async (req, res) => {
   }
 });
 
-// GET Book by ID
+// GET one Book by ID
 app.get('/books/:id', async (req, res) => {
   try {
     const { id } = req.params;
@@ -59,6 +59,30 @@ app.get('/books/:id', async (req, res) => {
     const book = await Book.findById(id);
 
     return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// UPDATE a book
+app.put('/books/:id', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.status(200).send({ message: 'Book updated successfully' });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
