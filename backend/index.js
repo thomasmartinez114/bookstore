32355dd7b5617717b0b45605cec3a562947e195e
Thomas Martinez <thomas.martinez@igt.com>
2024-02-12 20:48:05 -0500

    get book by id

--- backend/index.js
+++ backend/index.js
@@ -51,6 +51,20 @@ app.get('/books', async (req, res) => {
   }
 });
 
+// GET Book by ID
+app.get('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
