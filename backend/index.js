31ddebee8c289843e020bb737b98e861aa361aa7
Thomas Martinez <thomas.martinez@igt.com>
2024-02-12 19:44:30 -0500

    route to get books

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   return res.status(234).send('Welcome to Book Store');
 });
 
-// Route to save a new book
+// Route to POST a new book
 app.post('/books', async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
@@ -30,6 +30,21 @@ app.post('/books', async (req, res) => {
     const book = await Book.create(newBook);
 
     return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Route to GET books
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
