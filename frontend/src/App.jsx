e0318727545c5da7d2c3386877f2a51e9bdf3f48
Thomas Martinez <thomas.martinez@igt.com>
2024-02-13 20:12:53 -0500

    showing data and refactored code for the book table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,15 @@ import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/books/create' element={<CreateBook />} />
-    <Route path='/books/details/:id' element={<ShowBook />} />
-    <Route path='/books/edit/:id' element={<EditBook />} />
-    <Route path='/books/delete/:id' element={<DeleteBook />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
